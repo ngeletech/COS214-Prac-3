293897b5b7a579521834502f4ece6aba9906dd74
Madela_Nontokozo <nontokozomadela2@gmail.com>
2026-08-30 11:20:25 +0200

    Move composite headers into files folder

---
+++ files/EventGroup.h
@@ -0,0 +1,27 @@
+#ifndef EVENTGROUP_H
+#define EVENTGROUP_H
+
+#include <vector>
+#include "EventComponent.h"
+
+class EventGroup : public EventComponent
+{
+protected:
+    std::vector<EventComponent*> children;
+    
+public:
+    EventGroup() = default;
+
+    virtual void add(EventComponent* e) = 0;
+    virtual void remove(EventComponent* e) = 0;
+    virtual void open() = 0;
+    virtual void close() = 0;
+    virtual void reportStatus() = 0;
+    virtual int getCapacity() = 0;
+
+    ~EventGroup()  override;
+};
+
+
+
+#endif //EVENTGROUP_H
