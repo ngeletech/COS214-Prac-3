4ef2a6136e12f5e48b2997c3c72ac2d3d1d19957
ngele <ngele.mutwanamba@gmail.com>
2026-08-31 22:15:14 +0200

    EventUnit.h update with leaf implementation removed

--- files/EventUnit.h
+++ files/EventUnit.h
@@ -1,99 +1,19 @@
 #ifndef EVENTUNIT_H
 #define EVENTUNIT_H
 
-#include "EventComponent.h"
-#include <iostream>
 #include <string>
+#include "EventComponent.h"
 
 class EventUnit : public EventComponent
 {
 protected:
-    
     std::string name;
 
 public:
     explicit EventUnit(const std::string& unitName);
     virtual ~EventUnit() = default;
-    std::string getName() const;
-};
-
-class WaterRide : public EventUnit 
-{
-public:
-    void open() override;
-    void close() override;
-    void reportStatus() const override;
-    int getCapacity() const override;
-
-};
-
-class FirstAidStation : public EventUnit 
-{
-
-private:
-    std::string name;
-    int staffOnDuty;
-    bool isOperational;
-public:
-    FirstAidStation(const std::string& stationName, int staff);
-    ~FirstAidStation() override;
-
-
-    void open() override;
-    void close() override;
-    void reportStatus() const override;
-    int getCapacity() const override;
 
     std::string getName() const;
-    int getStaffOnDuty() const;
-    bool getIsOperational() const;
-
 };
-
-class TicketGate : public EventUnit 
-{
-private:
-    bool isOpen;
-    int capacity;
-    int profit;
-
-public:
-    TicketGate();
-    void open() override;
-    void close() override;
-    void reportStatus() const override;
-    int getCapacity() const override;
-    void orderTicket();
-    int moneyMade();
-
-};
-
-class FoodKiosk : public EventUnit 
-{
-private:
-    bool isOpen;
-    int capacity;
-    int profit;
-public:
-    FoodKiosk();
-    void open() override;
-    void close() override;
-    void reportStatus() const override;
-    int getCapacity() const override;
-    void orderingCustomer();
-    int moneyMade();
-
-};
-
-class KidsRide : public EventUnit 
-{
-public:
-    void open() override;
-    void close() override;
-    void reportStatus() const override;
-    int getCapacity() const override;
-
-};
-
 
 #endif //EVENTUNIT_H
