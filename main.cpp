0a6123d85f12778a6ae22a47b222c0eca46155b5
Madela_Nontokozo <nontokozomadela2@gmail.com>
2026-09-01 10:10:27 +0200

    Add Makefile file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "EventControl.h"
+#include "Land.h"
 
-int main() {
+int main () 
+{
+
+    EventControl control;
+    Land waterLand("NeverLand Park", true);
+
     return 0;
 }
