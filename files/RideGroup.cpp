8e8c5cf97e3f7e2e54628c6c41fbbe35831ab740
Madela_Nontokozo <nontokozomadela2@gmail.com>
2026-09-01 12:13:33 +0200

    Update RideGroup after merge

--- files/RideGroup.cpp
+++ files/RideGroup.cpp
@@ -53,26 +53,10 @@ void RideGroup::remove(EventComponent* e)
         if (children[i] == e)
         {
             children.erase(children.begin() + i);
-
-#include <vector>
-
-RideGroup::RideGroup(const std::string& groupName) : name(groupName) {}
-
-void RideGroup::add(EventComponent* e) {
-    if (e != nullptr) {
-        children.push_back(e);
-    }   
-}
-    
-void RideGroup::remove(EventComponent* e) {
-
-    for (auto it = children.begin(); it != children.end(); ++it) {
-        if (*it == e) {
-            children.erase(it)            break;
+            break;
         }
     }
 }
-
 
 void RideGroup::open()
 {
@@ -140,40 +124,3 @@ void transferUnit(EventComponent* unit, RideGroup& from, RideGroup& to)
 
     std::cout << "Unit transferred between ride groups." << std::endl;
 }
-
-    
-void RideGroup::open() {
-
-    for (EventComponent* child : children) {
-        child->open();
-    }
-}
-    
-void RideGroup::close() {
-    for (EventComponent* child : children) {
-        child->close();
-    }
-}
-    
-void RideGroup::reportStatus() const {
-
-    std::cout << "RideGroup status: " << std::endl;
-    for (EventComponent* child : children) {
-        child->reportStatus();
-    }
-}
-    
-int RideGroup::getCapacity() const {
-
-    int total = 0;
-    for (const EventComponent* child : children) {
-        total += child->getCapacity();
-    }
-}
-
-RideGroup::~RideGroup() {
-    for (EventComponent* child : children) {
-        delete child;
-    }
-    children.clear();
-} 
--- files/RideGroup.h
+++ files/RideGroup.h
@@ -3,7 +3,6 @@
 
 #include <iostream>
 #include "EventGroup.h"
-
 #include "Subject.h"
 #include "Observer.h"
 
@@ -113,24 +112,6 @@ public:
  * @param to Destination group.
  */
 void transferUnit(EventComponent* unit, RideGroup& from, RideGroup& to);
-
-
-class RideGroup : public EventGroup {
-private:
-    std::string name;
-public:
-    RideGroup(const std::string& groupName);
-    void add(EventComponent* e) override;
-    void remove(EventComponent* e) override;
-    void open() override;
-    void close() override;
-    void reportStatus() const;
-    int getCapacity() const;
-
-    ~RideGroup()  override;
-};
-
-
 
 
 #endif 
